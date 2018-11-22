44296e74f69299a7f327b72116e6e38c5c7cc611
Josh Diaz <josh@iserveproducts.com>
2018-11-21 20:00:55 -0700

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to my site.</p>
     <p>I'm in the middle of redesigning - please stop by again!</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+    {/* <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
-    </div>
+    </div> */}
   </Layout>
 )
 
