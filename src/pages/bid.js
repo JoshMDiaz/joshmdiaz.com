db7b0c78b56c5f88552b0943ab15e629901ced3f
Josh Diaz <josh@iserveproducts.com>
2018-11-25 13:08:10 -0700

    Adding layout for styles

--- src/pages/bid.js
+++ src/pages/bid.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Layout from '../components/layout'
 import Bid from '../components/Bid/Bid'
 
 const SecondPage = () => (
-  <Bid milesValue={.15} sqFtValue={.10} />
+  <Layout>
+    <Bid milesValue={.15} sqFtValue={.10} />
+  </Layout>
 )
 
 export default SecondPage
